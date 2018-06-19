8de701d6817685a9c03aeffbd052ea4cbf6d4467
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-06-19 18:39:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,5 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line_message = "The line is currently:"
+      katz_deli.each.with_index do |name, index|
+        line_message << " "
